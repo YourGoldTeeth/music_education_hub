66b4ae165fe9179c2cd166cf2cb069a6badd8143
James West <jameseswest@gmail.com>
2017-12-20 22:40:10 -0600

    start notifications

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -2,6 +2,9 @@ class HomepageController < ApplicationController
   
   def index
   	@resources = Resource.all
+  	@notifications = Assignment.where(user_id: current_user.id).order("created_at DESC")
   end
+
+
 
 end
